d9f55a2d914e12acb3efb4619a78d0fa5723944a
leminhtri1511 <trile20021511@gmail.com>
2024-08-07 20:33:00 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+namespace GiaiPhuongTrinhBacNhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, x;
+
+            Console.Write("a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            if (a == 0)
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("Phuong trinh vo so nghiem.");
+                }
+                else
+                {
+                    Console.WriteLine("Phuong trinh vo nghiem.");
+                }
+            }
+            else
+            {
+                 x = -b / a;
+                Console.WriteLine($"Nghiem cua phuong trinh la x = {x}");
+            }
+        }
+    }
+}
